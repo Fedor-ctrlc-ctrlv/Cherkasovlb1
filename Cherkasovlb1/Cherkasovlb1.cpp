4deb88a36093295a2b82ad692c7e0ac87165823c
Fedor-ctrlc-ctrlv <woodiquiker@gmail.com>
2024-10-18 16:19:12 +0300

    Second
    удалил класс трубы, начал все делать через структуры, описал структуру трубы

--- Cherkasovlb1/Cherkasovlb1.cpp
+++ Cherkasovlb1/Cherkasovlb1.cpp
@@ -1,7 +1,54 @@
 ﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+struct Pipe {
+	string name;
+	double length;
+	double diameter;
+	bool inRepair;
+	void input() {
+		cout << "Enter pipe`s name: ";
+		cin >> name;
+		cout << "Enter pipe`s length: ";
+		cin >> length;
+		cout << "Enter pipe`s diameter";
+		cin >> diameter;
+		inRepair = false;
+	}
+	void display() const {
+		cout << "Pipe: " << name << ", length: " << length << " km, Diameter: " << diameter << " m, In Repair: " << (inRepair ? "YES" : "NO") << endl;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	}
+	void Repair() {
+		inRepair = !inRepair;
+	}
+
+};
+struct CS {
+	string name;
+	int NumberofShops;
+	int RabochieShops;
+	double efficiency;
+
+	void input() {
+		cout << "Enter CS`s shop: ";
+		cin >> name;
+		cout << "Enter number of Shops: ";
+		cin >> NumberofShops;
+		RabochieShops = 0;
+		cout << "Enter efficiency";
+		cin >> efficiency;
+	}
+	void display() const {
+		cout << "CS: " << name < " ,Shops: " << NumberofShops << ", in works: " << RabochieShops << ", effectiveness" << efficiency << endl;
+	}
+
+};
+
+
+int main(){
+
 }
 
