094314a30e231c9b73702b7eefb9fcc7a4e50772
Fedor-ctrlc-ctrlv <woodiquiker@gmail.com>
2024-10-18 22:21:20 +0300

    The end
    Дописал меню, описал функцию сохранения и выгрузки данных из файла

--- Cherkasovlb1/Cherkasovlb1.cpp
+++ Cherkasovlb1/Cherkasovlb1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <string>
+
 using namespace std;
 struct Pipe {
 	string name;
@@ -64,19 +65,108 @@ struct CS {
 	}
 
 };
+void saveToFile() {
+	ofstream file("data.txt");
+	for (const auto& pipe : pipes) {
+		file << "Pipe " << pipe.name << " " << pipe.length << " " << pipe.diameter << " " << pipe.inRepair << "\n";
+	}
+	for (const auto& station : stations) {
+		file << "Station " << station.name << " " << station.NumberofShops << " " << station.RabochieShops << " " << station.efficiency << endl;
+	}
+	file.close();
+	cout << " Data have been saved\n";
+}
+void loadFromFile() {
+	ifstream file("data.txt");
+	string type;
+	while (file >> type) {
+		if (type == "Pipe") {
+			Pipe pipe;
+			file >> pipe.name >> pipe.length >> pipe.diameter >> pipe.inRepair;
+			pipes.push_back(pipe);
+		}
+		else if (type == "Station") {
+			CS station;
+			file >> station.name >> station.NumberofShops >> station.RabochieShops >> station.efficiency;
+			stations.push_back(station);
+		}
+	}
+	file.close();
+	cout << "data has been load from file";
+}
 vector<Pipe> pipes;
 vector<CS> stations;
 
-int main(){
+void menu() {
 	while (true) {
-		cout << "\n1. Add pipe\n2. Add CS\n3. view all objects\n4. Redact Pipe\n5. Redact CS\n6. Save\n7. Load\n8. EXIT";
+		cout << "\n1. Add pipe\n2. Add CS\n3. view all objects\n4. Redact Pipe\n5. Redact CS\n6. Save\n7. Load\n0. EXIT\n";
 		int choice;
 		cin >> choice;
 		switch (choice) {
 		case 1: {
-
+			Pipe pipe;
+			pipe.input();
+			pipes.push_back(pipe);
+			break;
 		}
+		case 2: {
+			CS station;
+			station.input();
+			stations.push_back(station);
+			break;
+		}
+		case 3:{
+			for (const auto& pipe : pipes)pipe.display();
+			for (const auto& station : stations) station.display();
+			break;
+		}
+		case 4: {
+			cout << "Enter pipe`s name to redact";
+			string name;
+			cin >> name;
+			for (auto& pipe : pipes) {
+				if (pipe.name == name) {
+					pipe.Repair();
+					cout << "The sign in the repair has been changed";
+					break;
+				}
+			}
+			break;
+		}
+		case 5: {
+			cout << "Enter CS`s name for redact";
+			string name;
+			cin >> name;
+			for (auto& station : stations) {
+				if (station.name == name) {
+					int action;
+					cout << "1. Start shop\n2. stop shop";
+					cin >> action;
+					if (action == 1)station.startShop();
+					else if (action == 2)station.stopShop();
+					break;
+				}
+			}
+			break;
+		}
+		case 6: {
+			saveToFile();
+			break;
+		}
+		case 7: {
+			loadFromFile();
+			break;
+		}
+		case 0:
+			return;
+		default:
+			cout << "the wrong number, try again\n";
 		}
 	}
+}
+
+int main(){
+	menu();
+	return 0;
 }
 
