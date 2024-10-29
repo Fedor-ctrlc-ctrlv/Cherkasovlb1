9806cddcc5e194b7cc632ed00e59dfa3e2b297e6
Fedor-ctrlc-ctrlv <woodiquiker@gmail.com>
2024-10-29 23:34:17 +0300

    29.10
    Изменил код, вынес функции за структуру, изменил формат записи в файл, так же формат ввода, убрал глобальные переменные, убрал вектора.

--- Cherkasovlb1/Cherkasovlb1.cpp
+++ Cherkasovlb1/Cherkasovlb1.cpp
@@ -1,224 +1,248 @@
 ﻿#include <iostream>
-#include <vector>
-#include <fstream>
 #include <string>
-
+#include <fstream>
 using namespace std;
-
 struct Pipe {
     string name;
-    double length;
-    double diameter;
-    bool inRepair;
-
-    void input() {
-        cin.ignore();
-        cout << "Enter pipe's name: ";
-        getline(cin, name);
-        cout << "Enter pipe's length: ";
-        cin >> length;
-        cout << "Enter pipe's diameter: ";
-        cin >> diameter;
-        inRepair = false;
-    }
-
-    void display() const {
-        cout << "Pipe: " << name << ", length: " << length << " km, Diameter: " << diameter << " m, In Repair: " << (inRepair ? "YES" : "NO") << endl;
-    }
-
-    void Repair() {
-        inRepair = !inRepair;
-    }
+    int length;
+    int diameter;
+    bool underRepair;
 };
 
-struct CS {
+struct PumpingStation {
     string name;
-    int NumberofShops;
-    int RabochieShops;
+    int totalShops;
+    int activeShops;
     double efficiency;
+}; 
+void inputPipe(Pipe& pipe) {
+    cout << "Enter pipe name: ";
+    cin.ignore();
+    getline(cin, pipe.name);
 
-    void input() {
-        cin.ignore(); 
-        cout << "Enter CS's name: ";
-        getline(cin, name);
-        cout << "Enter number of Shops (kol-vo Cehov): ";
-        cin >> NumberofShops;
-        RabochieShops = 0;
-        cout << "Enter efficiency: ";
-        cin >> efficiency;
+    cout << "Enter length (km): ";
+    while (!(cin >> pipe.length)) {
+        cout << "Invalid input. Please enter a number for length: ";
+        cin.clear();
+        cin.ignore(10000, '\n'); // Clear input buffer
     }
 
-    void display() const {
-        cout << "CS: " << name << ", Total shops: " << NumberofShops << ", in works: " << RabochieShops << ", effectiveness: " << efficiency << endl;
+    cout << "Enter diameter: ";
+    while (!(cin >> pipe.diameter)) {
+        cout << "Invalid input. Please enter a number for diameter: ";
+        cin.clear();
+        cin.ignore(10000, '\n'); // Clear input buffer
     }
 
-    void startShop() {
-        if (RabochieShops < NumberofShops) {
-            RabochieShops++;
-            cout << "Shop started working" << endl;
+    char repairStatus;
+    cout << "Is the pipe under repair? (y/n): ";
+    cin >> repairStatus;
+    pipe.underRepair = (repairStatus == 'y' || repairStatus == 'Y');
+}
+
+void inputPumpingStation(PumpingStation& ps) {
+    cout << "Enter pumping station name: ";
+    cin.ignore();
+    getline(cin, ps.name);
+
+    cout << "Enter total number of shops: ";
+    while (!(cin >> ps.totalShops)) {
+        cout << "Invalid input. Please enter a number for total shops: ";
+        cin.clear();
+        cin.ignore(10000, '\n'); // Clear input buffer
+    }
+
+    cout << "Enter number of active shops: ";
+    while (!(cin >> ps.activeShops)) {
+        cout << "Invalid input. Please enter a number for active shops: ";
+        cin.clear();
+        cin.ignore(10000, '\n'); // Clear input buffer
+    }
+
+    cout << "Enter efficiency: ";
+    while (!(cin >> ps.efficiency)) {
+        cout << "Invalid input. Please enter a number for efficiency: ";
+        cin.clear();
+        cin.ignore(10000, '\n'); // Clear input buffer
+    }
+}
+
+void displayPipe(const Pipe& pipe) {
+    cout << "Pipe Name: " << pipe.name << "\n"
+        << "Length: " << pipe.length << " km\n"
+        << "Diameter: " << pipe.diameter << "\n"
+        << "Under Repair: " << (pipe.underRepair ? "Yes" : "No") << "\n";
+}
+
+void displayPumpingStation(const PumpingStation& ps) {
+    cout << "Pumping Station Name: " << ps.name << "\n"
+        << "Total Shops: " << ps.totalShops << "\n"
+        << "Active Shops: " << ps.activeShops << "\n"
+        << "Efficiency: " << ps.efficiency << "\n";
+}
+
+void editPipeRepairStatus(Pipe& pipe) {
+    char repairStatus;
+    cout << "Change repair status for pipe " << pipe.name << " (y/n): ";
+    cin >> repairStatus;
+    pipe.underRepair = (repairStatus == 'y' || repairStatus == 'Y');
+}
+
+void editPumpingStationShops(PumpingStation& ps) {
+    char action;
+    cout << "Do you want to activate (a) or deactivate (d) a shop? ";
+    cin >> action;
+
+    if (action == 'a' || action == 'A') {
+        if (ps.activeShops < ps.totalShops) {
+            ps.activeShops++;
+            cout << "One shop activated.\n";
         }
         else {
-            cout << "ALL SHOPS ARE WORKING" << endl;
+            cout << "All shops are already active.\n";
         }
     }
-
-    void stopShop() {
-        if (RabochieShops > 0) {
-            RabochieShops--;
-            cout << "Shop stopped" << endl;
+    else if (action == 'd' || action == 'D') {
+        if (ps.activeShops > 0) {
+            ps.activeShops--;
+            cout << "One shop deactivated.\n";
         }
         else {
-            cout << "No working shop" << endl;
+            cout << "No active shops to deactivate.\n";
         }
     }
-};
-
-vector<Pipe> pipes;
-vector<CS> stations;
-
-void saveToFile() {
-    ofstream file("data.txt");
-    for (const auto& pipe : pipes) {
-        file << "Pipe " << pipe.name << " " << pipe.length << " " << pipe.diameter << " " << pipe.inRepair << "\n";
+    else {
+        cout << "Invalid action.\n";
     }
-    for (const auto& station : stations) {
-        file << "Station " << station.name << " " << station.NumberofShops << " " << station.RabochieShops << " " << station.efficiency << endl;
+}
+void view_all(const Pipe& pipe, const PumpingStation& ps) {
+    if (pipe.name == "None") {
+        cout << "Pipe isn`t defined\n";
     }
-    file.close();
-    cout << "Data has been saved\n";
+    else {
+        displayPipe(pipe);
+    };
+    if (ps.name == "None") {
+        cout << "PS isn`t defined\n";
+    }
+    else {
+        displayPumpingStation(ps);
+    };
 }
 
-void loadFromFile() {
-    ifstream file("data.txt");
-    string type;
-    while (file >> type) {
-        if (type == "Pipe") {
-            Pipe pipe;
-            file >> ws; 
-            getline(file, pipe.name, ' ');
-            file >> pipe.length >> pipe.diameter >> pipe.inRepair;
-            pipes.push_back(pipe);
-        }
-        else if (type == "Station") {
-            CS station;
-            file >> ws;
-            getline(file, station.name, ' ');
-            file >> station.NumberofShops >> station.RabochieShops >> station.efficiency;
-            stations.push_back(station);
-        }
-    }
-    file.close();
-    cout << "Data has been loaded from file\n";
+void pipe_in_file(const Pipe& pipe, ofstream& file) {
+    file << "Pipe\n" <<
+        pipe.name << endl <<
+        pipe.length << endl <<
+        pipe.diameter << endl <<
+        pipe.underRepair << endl;
 }
 
-void menu() {
-    while (true) {
-        cout << "\n1. Add pipe\n2. Add CS\n3. View all objects\n4. Edit Pipe\n5. Edit CS\n6. Save\n7. Load data\n0. EXIT\n";
-        int choice;
-        cin >> choice;
-        switch (choice) {
-        case 1: {
-            Pipe pipe;
-            pipe.input();
-            pipes.push_back(pipe);
-            break;
+void ps_in_file(const PumpingStation& ps, ofstream& file) {
+    file << "Pump Station\n" <<
+        ps.name << endl <<
+        ps.totalShops << endl <<
+        ps.activeShops << endl <<
+        ps.efficiency << endl;
+}
+
+void saveToFile(Pipe& pipe, PumpingStation& ps) {
+    ofstream file;
+    file.open("data.txt");
+    if (!file.is_open()) {
+        cout << "File is`nt defined";
+    }
+    else {
+        if (!(pipe.name == "None")) {
+            pipe_in_file(pipe, file);
         }
-        case 2: {
-            CS station;
-            station.input();
-            stations.push_back(station);
-            break;
-        }
-        case 3: {
-            for (const auto& pipe : pipes) pipe.display();
-            for (const auto& station : stations) station.display();
-            break;
-        }
-        case 4: {
-            cout << "Enter pipe's name to edit: " << endl;
-            cin.ignore();
-            string name;
-            getline(cin, name);
-            for (auto& pipe : pipes) {
-                if (pipe.name == name) {
-                    cout << "Enter number for 1 to 3\n1. Change length\n2. Change diameter\n3. Change status of Repair\n";
-                    int minichoice;
-                    cin >> minichoice;
-                    switch (minichoice) {
-                    case 1: {
-                        cout << "Enter new length: " << endl;
-                        cin >> pipe.length;
-                        break;
-                    }
-                    case 2: {
-                        cout << "Enter new diameter: ";
-                        cin >> pipe.diameter;
-                        break;
-                    }
-                    case 3: {
-                        cout << "Change status of Repair ";
-                        pipe.Repair();
-                        break;
-                    }
-                    default:
-                        cout << "The wrong number, please try again" << endl;
-                    }
-                    cout << "The repair status has been changed" << endl;
-                    break;
-                }
-            }
-            break;
-        }
-        case 5: {
-            cout << "Enter CS's name to edit: " << endl;
-            cin.ignore();
-            string name;
-            getline(cin, name);
-            for (auto& station : stations) {
-                if (station.name == name) {
-                    int action;
-                    cout << "Enter number for 1 to 3\n1. Start shop\n2. Stop shop\n3. Change number of shops\n";
-                    cin >> action;
-                    switch (action) {
-                    case 1: {
-                        station.startShop();
-                        break;
-                    }
-                    case 2: {
-                        station.stopShop();
-                        break;
-                    }
-                    case 3: {
-                        cout << "Enter new number of shops: ";
-                        cin >> station.NumberofShops;
-                        break;
-                    }
-                    default:
-                        cout << "The wrong number, try again\n";
-                    }
-                }
-            }
-            break;
-        }
-        case 6: {
-            saveToFile();
-            break;
-        }
-        case 7: {
-            loadFromFile();
-            break;
-        }
-        case 0:
-            return;
-        default:
-            cout << "The wrong number, try again\n";
+        if (!(ps.name == "None")) {
+            ps_in_file(ps, file);
         }
     }
 }
-
+void info_pipe_fr_file(Pipe& pipe, ifstream& file) {
+    getline(file, pipe.name);
+    file >> pipe.length;
+    file >> pipe.diameter;
+    file >> pipe.underRepair;
+}
+void info_ps_fr_file(PumpingStation& ps, ifstream& file) {
+    getline(file, ps.name);
+    file >> ps.totalShops;
+    file >> ps.activeShops;
+    file >> ps.efficiency;
+}
+void loadFromFile(Pipe& pipe, PumpingStation& ps) {
+    string line;
+    ifstream file;
+    file.open("data.txt");
+    if (!file.is_open()) {
+        cout << "File isn`t defined";
+        return;
+    }
+    while (getline(file, line)) {
+        if (line == "Pipe") {
+            info_pipe_fr_file(pipe, file);
+        }
+        if (line == "Pump Station") {
+            info_ps_fr_file(ps, file);
+        }
+    }
+}
 int main() {
-    menu();
+    Pipe pipe = {"None"};
+    PumpingStation ps= { "None"};
+    int choice;
+
+    do {
+        cout << "\nMenu:\n"
+            << "1. Add Pipe\n"
+            << "2. Add Pumping Station\n"
+            << "3. View All Objects\n"
+            << "4. Edit Pipe\n"
+            << "5. Edit Pumping Station\n"
+            << "6. Save to File\n"
+            << "7. Load from File\n"
+            << "0. Exit\n"
+            << "Choose an action: ";
+
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            inputPipe(pipe);
+            break;
+        case 2:
+            inputPumpingStation(ps);
+            break;
+        case 3:
+            view_all(pipe,ps);
+            break;
+        case 4:
+            editPipeRepairStatus(pipe);
+            break;
+        case 5:
+            editPumpingStationShops(ps);
+            break;
+        case 6:
+            saveToFile(pipe, ps);
+            break;
+        case 7:
+            loadFromFile(pipe, ps);
+            break;
+        case 0:
+            cout << "Exiting the program.\n";
+            break;
+        default:
+            cout << "Invalid choice. Please try again.\n";
+        }
+    } while (choice != 0);
+
     return 0;
 }
+
+
+
 
 
